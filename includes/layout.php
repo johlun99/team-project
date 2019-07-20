e80f9e48c26761f1342d9dd7c8b86bb4a16a464a
Johan Lundgren <lundgren.johan99@gmail.com>
2019-07-20 15:26:13 +0200

    Added JQuery 3.4.1¨

--- includes/layout.php
+++ includes/layout.php
@@ -1,6 +1,6 @@
 <?php
 define("START", "<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'>");
-define("CSS", "");
+define("CSS", "<link rel='stylesheet' href='bootstrap-4.1.3-dist/css/bootstrap.css'>");
 define("BODY", "</head><body>");
-define("JS", "");
+define("JS", "<script src='bootstrap-4.1.3-dist/js/bootstrap.js'></script>");
 define("END", "</body></html>");
